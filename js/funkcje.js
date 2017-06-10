97e69cef3f12cd02d559427c070a640ea885e931
Dominika Gnich <d.gnich@wp.pl>
2017-06-10 15:09:04 +0200

    funkcje

---
+++ js/funkcje.js
@@ -0,0 +1,38 @@
+
+
+//function multiply(parametr1, parametr2, parametr3){
+    'use strict';
+//    'use strict';
+//    
+//    var result;
+//    
+//    result = parametr1 * parametr2 * parametr3;
+//    
+//    return result;
+//}
+//
+//var wynik = multiply( 3, 9, 10);
+//
+//console.log("wynik: " + wynik);
+//
+//  wynik = multiply( 35, 2, 10);
+//
+//console.log("wynik: " + wynik);
+
+//funkcje aninimowe, przypisujemy do zmiennej funkcje
+
+var funkcja = function (){
+    return "wynik :)";
+}
+
+console.log( funkcja); //traktuje funkcje jako stringa
+console.log( funkcja()); // wykonuje funkcje
+
+
+
+
+
+
+
+
+
